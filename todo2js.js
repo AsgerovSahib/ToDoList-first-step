3fced02ca42937edbb00a08e074fb50a86c85779
AsgerovSahib <asgerov.sako@gmail.com>
2023-08-07 18:19:42 +0400

    add innerHTML

--- todo2js.js
+++ todo2js.js
@@ -1,39 +1,21 @@
-const input =document.querySelector("#input");
-let show=document.querySelector("#show");
-let send=document.querySelector("#send");
-let arr=[];
+const input = document.querySelector("#input");
+let show = document.querySelector("#show");
+let send = document.querySelector("#send");
+let list = document.querySelector("#list");
+let arr = [];
 
+send.addEventListener("click", function (e) {
+  let data = input.value;
+  arr.push(data);
+  // console.log(data);
+  input.value = "";
+  // console.log(arr)
+});
+show.addEventListener("click", function (e) {
+  for (let i = 0; i < arr.length; i++) {}
 
-send.addEventListener("click",function (e) {
-    let data=input.value;
-    arr.push(data);
-    // console.log(data);
-    input.value="";
-    // console.log(arr)
-})
-show.addEventListener("click",function(e){
-    for(let i=0;i<arr.length;i++){
-        
-    }
-
-    const newContent = arr
-    .map(
-      (todo, index) =>
-        `<li onclick="removeToDo(${index})">${index + 1}. ${todo} </li>`
-    )
+  const newContent = arr
+    .map((a, index) => `<li>${index + 1}. ${a} </li>`)
     .join("");
-        
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
+  list.innerHTML = newContent;
+});
